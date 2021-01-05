4b5a63db718f7cfd0133384eb33bd6985b4f0ee2
Summer <su943515688@gmail.com>
2021-01-05 14:29:56 +0800

    [*] ~~~~

--- 350.两个数组的交集II/350.两个数组的交集II.go
+++ 350.两个数组的交集II/350.两个数组的交集II.go
@@ -1,6 +1,6 @@
 package leetcode
 
-func intersect(nums1 []int, nums2 []int) []int {
+func intersect0(nums1 []int, nums2 []int) []int {
 	m := map[int]int{}
 	for _, v := range nums1 {
 		m[v] += 1
@@ -15,4 +15,23 @@ func intersect(nums1 []int, nums2 []int) []int {
 	}
 
 	return inters
+}
+
+func intersect1(nums1 []int, nums2 []int) []int {
+	m := map[int]int{}
+	for _, v := range nums1 {
+		m[v] += 1
+	}
+
+	k := 0
+	for _, v := range nums2 {
+		if m[v] > 0 {
+			m[v] -= 1
+			// 把元素存在nums2中节省内存
+			nums2[k] = v
+			k++
+		}
+	}
+
+	return nums2[0:k]
 }
--- 350.两个数组的交集II/350.两个数组的交集II_test.go
+++ 350.两个数组的交集II/350.两个数组的交集II_test.go
@@ -6,12 +6,12 @@ import (
 	"github.com/go-playground/assert/v2"
 )
 
-func TestIntersect(t *testing.T) {
+func TestIntersect0(t *testing.T) {
 	num1 := []int{1, 2, 2, 1}
 	num2 := []int{2, 2, 2, 3}
 	except := []int{2, 2}
 
-	result := intersect(num1, num2)
+	result := intersect0(num1, num2)
 
 	assert.Equal(t, result, except)
 }
