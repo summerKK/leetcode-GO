fb027dea4ab73223903b2883158e8c7d51c168fc
Summer <su943515688@gmail.com>
2021-01-05 14:51:39 +0800

    [+] 添加新的解法

--- 350.两个数组的交集II/350.两个数组的交集II.go
+++ 350.两个数组的交集II/350.两个数组的交集II.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "sort"
+
 func intersect0(nums1 []int, nums2 []int) []int {
 	m := map[int]int{}
 	for _, v := range nums1 {
@@ -34,4 +36,28 @@ func intersect1(nums1 []int, nums2 []int) []int {
 	}
 
 	return nums2[0:k]
+}
+
+func intersect2(nums1 []int, nums2 []int) []int {
+	var i, j, k int
+	// 先对数组进行排序
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+
+	for i < len(nums1) && j < len(nums2) {
+		// 两个数组元素相同.放入到空白数组(这里使用nums1来存放,节省内存.没放一个元素k++)
+		if nums1[i] == nums2[j] {
+			nums1[k] = nums1[i]
+			i++
+			j++
+			k++
+		} else if nums1[i] > nums2[j] {
+			// 如果两个数组的元素不同,元素小的那个数组往前移动一位.元素大的那个数组保持不变
+			j++
+		} else {
+			i++
+		}
+	}
+
+	return nums1[0:k]
 }
--- 350.两个数组的交集II/350.两个数组的交集II_test.go
+++ 350.两个数组的交集II/350.两个数组的交集II_test.go
@@ -15,3 +15,13 @@ func TestIntersect0(t *testing.T) {
 
 	assert.Equal(t, result, except)
 }
+
+func TestIntersect2(t *testing.T) {
+	num1 := []int{1, 2, 3, 4, 4, 13}
+	num2 := []int{1, 2, 3, 9, 10}
+	except := []int{1, 2, 3}
+
+	result := intersect2(num1, num2)
+
+	assert.Equal(t, result, except)
+}
