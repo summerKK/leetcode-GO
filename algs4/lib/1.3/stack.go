f0d8398a7261e9ff9b454e0bf058367202d7ee07
Summer <su943515688@gmail.com>
2021-01-27 10:35:27 +0800

    [+] algs4 1.3 stack

--- algs4/lib/1.3/stack.go
+++ algs4/lib/1.3/stack.go
@@ -23,7 +23,6 @@ func (f *FixedCapStack) Push(item interface{}) {
 	// 达到数组最大容量
 	if f.N == len(f.a) {
 		f.resize(len(f.a) * 2)
-		fmt.Printf("stack resize:%d\n", len(f.a)*2)
 	}
 	f.a[f.N] = item
 	f.N++
@@ -32,6 +31,13 @@ func (f *FixedCapStack) Push(item interface{}) {
 func (f *FixedCapStack) Pop() interface{} {
 	f.N--
 	item := f.a[f.N]
+	// 更好GC
+	f.a[f.N] = nil
+	// 数组容量过大,减小容量
+	if f.N > 0 && len(f.a)/4 > f.N {
+		// 容量减半
+		f.resize(len(f.a) / 2)
+	}
 
 	return item
 }
@@ -46,9 +52,28 @@ func (f *FixedCapStack) Size() int {
 
 // 重置数组大小
 func (f *FixedCapStack) resize(max int) {
+	fmt.Printf("stack resize:%d\n", max)
+
 	arr := make([]interface{}, max)
 	for i := 0; i < f.Size(); i++ {
 		arr[i] = f.a[i]
 	}
 	f.a = arr
+}
+
+func (f *FixedCapStack) Loop() <-chan interface{} {
+	c := make(chan interface{})
+	go func() {
+		for !f.IsEmpty() {
+			c <- f.next()
+		}
+		close(c)
+	}()
+
+	return c
+}
+
+func (f *FixedCapStack) next() interface{} {
+	f.N--
+	return f.a[f.N]
 }
--- algs4/lib/1.3/stack_test.go
+++ algs4/lib/1.3/stack_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-var S Stack
+var S *FixedCapStack
 
 func TestMain(m *testing.M) {
 	S = &FixedCapStack{}
@@ -14,7 +14,7 @@ func TestMain(m *testing.M) {
 	m.Run()
 }
 
-func TestPush(t *testing.T) {
+func TestFixedCapStack_Push(t *testing.T) {
 	input := "to be or not to - be - - that - - - is"
 	inputs := strings.Split(input, " ")
 	for _, input := range inputs {
@@ -23,5 +23,17 @@ func TestPush(t *testing.T) {
 		} else {
 			S.Push(input)
 		}
+	}
+}
+
+func TestFixedCapStack_Loop(t *testing.T) {
+	input := "to be or not to be that is"
+	inputs := strings.Split(input, " ")
+	for _, s := range inputs {
+		S.Push(s)
+	}
+
+	for v := range S.Loop() {
+		fmt.Printf("%v ", v)
 	}
 }
