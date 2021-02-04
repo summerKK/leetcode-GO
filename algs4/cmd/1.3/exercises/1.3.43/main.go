d01826ac468a6550f81e1302651da10d82385e3b
Summer <su943515688@gmail.com>
2021-02-04 16:01:55 +0800

    [+] algs4 1.3 课后习题

---
+++ algs4/cmd/1.3/exercises/1.3.43/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/fatih/color"
+	lib "github.com/summerKK/leetcode-Go/algs4/lib/1.3"
+)
+
+var colo *color.Color
+
+/**
+1.3.43
+文件列表。文件夹就是一列文件和文件夹的列表。编写一个程序，从命令行接受一个文件夹名
+作为参数，打印出该文件夹下的所有文件并用递归的方式在所有子文件夹的名下(缩进)列出
+其下的所有文件。提示:使用队列，并参考 java.io.File。
+*/
+func main() {
+	colo = color.New(color.FgYellow)
+	queue := &lib.MyQueue{}
+	_ = dir("/Users/summer/Docker/go-summer/leetcode-Go", queue, -1)
+}
+
+func dir(path string, queue *lib.MyQueue, depth int) (err error) {
+	depth++
+	file, err := os.Open(path)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return
+	}
+
+	level := ""
+	for i := 0; i < depth; i++ {
+		level += fmt.Sprint(" ")
+	}
+	fileName := fileInfo.Name()
+	if fileInfo.IsDir() {
+		fileName += "~~"
+	}
+	queue.Enqueue(level + fileName)
+
+	if fileInfo.IsDir() {
+		fileInfos, err := file.Readdir(0)
+		if err != nil {
+			return err
+		}
+		for _, fileInfo := range fileInfos {
+			err = dir(path+"/"+fileInfo.Name(), queue, depth)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	for fileName := queue.Dequeue(); fileName != nil; fileName = queue.Dequeue() {
+		if strings.Contains(fileName.(string), "~~") {
+			_, _ = colo.Println(strings.TrimRight(fileName.(string), "~~"))
+		} else {
+			fmt.Println(fileName)
+		}
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/summerKK/leetcode-Go
 
 go 1.13
 
-require github.com/go-playground/assert/v2 v2.0.1
+require (
+	github.com/fatih/color v1.10.0
+	github.com/go-playground/assert/v2 v2.0.1
+)
